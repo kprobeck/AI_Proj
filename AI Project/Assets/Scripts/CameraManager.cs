01f83f08ca9ceb4cc84711a16622ec2715bbf3df
Kenneth Probeck <kxp4109@g.rit.edu>
2017-02-16 16:15:00 -0500

    project 0.0.1
    Added all the things.

---
+++ AI Project/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+    [SerializeField] private Camera worldCamera;
+    [SerializeField] private Camera personCamera;
+
+
+	// Use this for initialization
+	void Start () {
+        worldCamera.enabled = false;
+        personCamera.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            worldCamera.enabled = !worldCamera.enabled;
+            personCamera.enabled = !personCamera.enabled;
+        }
+    }
+}
