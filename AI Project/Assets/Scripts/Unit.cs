ec664dda6d12292b6a26e1fb09d5107099a2ba18
Kenneth Probeck (RIT Student) <kxp4109@ad.rit.edu>
2017-04-17 18:05:12 -0400

    added new scripts for Unit and WorldInput, also added new variables in the world scene so we can test them

---
+++ AI Project/Assets/Scripts/Unit.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+
+    // properties - TODO: edit if they should be public or not, all public now for testing
+        
+        // affiliation, represents the actual influence the unit provides, scale form 1 - 4
+    public int affiliation;
+
+        // boolean to see if on red or green team, TRUE = RED, FALSE = GREEN
+    public bool isRedTeam;
+
+    public Vector2 location;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // TODO: determineTeam function, checks location to determine what team they are one
+    void determineTeam() {
+        // if locationX is 0 - 4, red
+        isRedTeam = true;
+
+        // if locationX is 5-9, green
+        isRedTeam = false;
+    }
+}
---
+++ AI Project/Assets/Scripts/WorldInput.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldInput : MonoBehaviour {
+
+    // properties - TODO: edit if they should be public or not, all public now for testing
+
+    // units array, collection of all units in the world
+    public Unit[] units;
+
+    // unitToPlace, determines what type of unit to place (what their affiliation should be)
+    public Unit unitToPlace;
+
+    // Use this for initialization
+    void Start () {
+        unitToPlace = new Unit();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        checkAffiliation();
+	}
+
+    // see what unit to place based on input, 1, 2, 3, 4 will provide what affiliation to have
+    void checkAffiliation() {
+        if (Input.GetKeyDown("1")) {
+            unitToPlace.affiliation = 1;
+            return;
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            unitToPlace.affiliation = 2;
+            return;
+        }
+
+        if (Input.GetKeyDown("3"))
+        {
+            unitToPlace.affiliation = 3;
+            return;
+        }
+
+        if (Input.GetKeyDown("4"))
+        {
+            unitToPlace.affiliation = 4;
+            return;
+        }
+    }
+}
